788945260fa22abaf64ec43f8231a260db489497
Sai Vikranth Desu <vikranthdesu@gmail.com>
2022-01-11 14:45:47 -0500

    Add a viewmodel for the main activity.

--- app/src/main/java/ca/chronofit/chrono/MainActivity2.kt
+++ app/src/main/java/ca/chronofit/chrono/MainActivity2.kt
@@ -2,8 +2,10 @@ package ca.chronofit.chrono
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -12,14 +14,25 @@ import ca.chronofit.chrono.databinding.ActivityMain2Binding
 class MainActivity2 : AppCompatActivity() {
 
     private lateinit var bind: ActivityMain2Binding
+    private lateinit var viewModel: MainViewModel
+
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        splashScreen()
+
         bind = DataBindingUtil.setContentView(this, R.layout.activity_main2)
 
         setupNavigation()
+    }
+
+    private fun splashScreen() {
+        val splash = installSplashScreen()
+        splash.setKeepVisibleCondition{
+            viewModel.initialize()
+        }
     }
 
     private fun setupNavigation() {
---
+++ app/src/main/java/ca/chronofit/chrono/MainViewModel.kt
@@ -0,0 +1,16 @@
+package ca.chronofit.chrono
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    init {
+        //TODO: We'll have to add stuff in here later.
+    }
+
+
+    fun initialize(): Boolean {
+        TODO("Not yet implemented")
+        return true
+    }
+}
